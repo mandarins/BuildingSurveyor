8f484602e19d970ce063b153f653801c39cf2cd1
Salvatore Lentini <mandarins@gmail.com>
2018-10-05 19:03:57 -0400

    Initial Commit

---
+++ BuildingSurveyor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuildingSurveyor
+//
+//  Created by Salvatore Lentini on 10/5/18.
+//  Copyright © 2018 Salvatore Lentini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
